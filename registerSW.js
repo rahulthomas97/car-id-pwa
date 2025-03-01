2abdf28db12320a795781f155a6a8fc0823d13d5
rahul <rahulthomas97@gmail.com>
2025-03-01 00:04:37 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/car-id-pwa/sw.js', { scope: '/car-id-pwa/' })})}
